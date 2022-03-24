0d370c7e99a1b033983066a230a04d5742ef304b
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-03-24 19:21:05 +0900

    - 내부조인 외부조인 test

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,19 +13,29 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member1 = new Member();
-            member1.setAge(10);
-            member1.setUserName("김김");
-            em.persist(member1);
+            Team team = new Team();
+            team.setName("team1");
+            em.persist(team);
 
-            List<MemberDTO> results = em.createQuery("select new jpql.MemberDTO(m.userName, m.age) from Member m", MemberDTO.class)
+            Member member = new Member();
+            member.setUserName("홍길동");
+            member.setAge(12);
+            member.addTeam(team);
+            em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUserName("김길동");
+            member2.setAge(13);
+
+            em.flush();
+            em.clear();
+            System.out.println("==============");
+            String query = "select m from Member m inner join m.team t where m.age=12";
+            List<Member> results = em.createQuery(query, Member.class)
                     .getResultList();
-            MemberDTO memberDTO = results.get(0);
-            System.out.println(memberDTO.getUserName());
-            System.out.println(memberDTO.getAge());
-
-
-
+            for (Member member1 : results){
+                System.out.println(member1.getUserName());
+            }
             tx.commit();
         }catch (Exception e){
             e.printStackTrace();
